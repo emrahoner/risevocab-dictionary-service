688c8af0711d1b5e7f8b22f1bd26a6d1c893d144
Emrah Oner <emrahoner@gmail.com>
2020-03-26 07:00:19 +1100

    Update port to be exposed in local docker

--- build-run-docker.sh
+++ build-run-docker.sh
@@ -4,4 +4,4 @@ if [ "$(docker ps -aq -f name=$1)" ]; then
     docker rm --force $1
 fi
 docker image build -f $3 -t $1:local .
-docker run --publish $2:4000 --detach --name $1 $1:local
+docker run --publish $2:8001 --detach --name $1 $1:local
